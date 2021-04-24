b927571ca7b85d21834d5789f8255ef9a541974c
xiyanxy <xiyanxyz@qq.com>
2021-04-24 12:29:10 +0800

    v798

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 #### cemu模拟器图像插件graphicPacks汉化
 
-##### 版本：V764
+##### 版本：V798
 
 ##### 汉化作者：曦颜XY(微博：曦颜XY)
 
-##### 更新时间：2021.02.25
+##### 更新时间：2021.04.24
 
 ##### 说明：
 
 1. 部分游戏本人没有玩过，例如**异度之刃X，汉化并完善**，希望这游戏会玩的同学可以联系协助汉化
 2. 汉化可能存在纰漏，若发现汉化某些部分不对，请联系本人进行修正
 3. 剔除虚拟终端、WiiU eShop
-4. 文件相关权利见：[**Cemu Graphic Packs**](https://slashiee.github.io/cemu_graphic_packs/)
+4. 文件相关权利见：[**Cemu Graphic Packs**](https://github.com/ActualMandM/cemu_graphic_packs)
 5. 没有汉化的官网图像插件请见：**graphicPacks_English**
 6. **请勿作为商用**，**请勿作为商用**，**请勿作为商用**，**淘宝和闲鱼奸商请知悉！**
 7. 汉化包补全原版每个插件的作者相关信息，尊重版权！
---
+++ graphicPacks_English/SonicBoomRiseOfLyric/Mods/DisableIntroLogos/patch_DisableIntroLogos.asm
@@ -0,0 +1,35 @@
+[WiiULauncher0US]
+moduleMatches = 0x90DAC5CE
+
+; Jump to title from Japanese video
+0x34BA068 = b 0x34BA2C8
+
+; Jump to title from English video
+0x34BA074 = b 0x34BA2C8
+
+[WiiULauncher0EU]
+moduleMatches = 0x8F7D2702
+
+; Jump to title from Japanese video
+0x34BA0F0 = b 0x34BA350
+
+; Jump to title from English video
+0x34BA0FC = b 0x34BA350
+
+[WiiULauncher0JP]
+moduleMatches = 0x0D395735
+
+; Jump to title from Japanese video
+0x34BA430 = b 0x34BA690
+
+; Jump to title from English video
+0x34BA43C = b 0x34BA690
+
+[WiiULauncher16]
+moduleMatches = 0x113CC316
+
+; Jump to title from Japanese video
+0x34BA4B8 = b 0x34BA760
+
+; Jump to title from English video
+0x34BA4C4 = b 0x34BA760
